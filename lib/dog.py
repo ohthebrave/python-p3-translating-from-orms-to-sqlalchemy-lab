1925e2b006660f6ba7d78aedcf685ab7f3405719
George <georgejoj28@gmail.com>
2023-12-08 13:37:24 +0300

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,34 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
 from models import Dog
 
-def create_table(base):
-    pass
+
+
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    # Use .first() to get the first result or .all() to get all results
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    # Use .first() to get the first result or .all() to get all results
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    # Use multiple filter conditions to compare each attribute
+    return session.query(Dog).filter(Dog.name == name, Dog.breed ==  breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    # Update the breed attribute of the Dog instance
+    dog.breed = breed
+    
+    # Commit the changes to the database
+    session.commit()
